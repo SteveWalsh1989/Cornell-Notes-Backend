0e42f7853cda026c9e918da662c7e7d52aadb1a0
Stevewalsh1989 <stevewalsh89@gmail.com>
2020-01-01 20:26:32 +0000

    Inital Commit - basic swagger initalisation

---
+++ Proto_1/main/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"github.com/go-openapi/loads"
+)
+
+func main() {
+
+	// Initialize Swagger
+	swaggerSpec, err := loads.Analyzed(restapi.SwaggerJSON, "")
+	if err != nil {
+	  log.Fatalln(err)
+	}
+	api := operations.NewHelloAPI(swaggerSpec)
+	server := restapi.NewServer(api)
+	defer server.Shutdown()
+	server.Port = 8080
+	// Start listening using having the handlers and port
+	// already set up.
+	if err := server.Serve(); err != nil {
+	  log.Fatalln(err)
+	}
+  
+
+}
