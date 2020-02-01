b2cfa130263c0801ecd67990cdc65a0d2d096531
Stevewalsh1989 <stevewalsh89@gmail.com>
2020-02-01 15:29:31 +0000

    pdf struct

--- model/pdf.go
+++ model/pdf.go
@@ -8,6 +8,7 @@ import (
 type PDF struct {
 	ID          string    `db:"id" json:"ID"`
 	Name        string    `db:"name" json:"Name"`
+	pdfContent  []byte    `db:"pdfContent" json:"pdfContent"`
 	Status      string    `db:"status" json:"Status"`
 	DateCreated time.Time `db:"date_created" json:"DateCreated"`
 	DateEdited  time.Time `db:"date_edited" json:"DateEdited"`
